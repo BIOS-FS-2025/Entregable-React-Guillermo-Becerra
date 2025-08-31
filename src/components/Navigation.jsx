a3e114a5e8c043833f840c965b423fc736725333
guillermobecerra <guillebvb@gmail.com>
2025-08-31 01:07:25 -0300

    navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,38 @@
 import { Link } from "react-router-dom";
 
-function Navigation () {
+function Navigation() {
     return (
-        <nav>
-            <div>
-                <div>
-                    <span>Logo</span>
-                </div>
+        <nav className="bg-gray-900 shadow-md">
+            <div className="pl-8 pr-12">
+                <div className="flex justify-between h-20 items-center">
 
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/card">Card Detail</Link>
-                    </li>
-                </ul>
+                    {/* Logo */}
+                    <Link to="/">
+                        <div className="flex-shrink-0">
+                            <span className="text-xl tracking-wide font-bold text-yellow-400">KAIBAPEDIA</span>
+                        </div>
+                    </Link>
+
+                    {/* Links */}
+                    <ul className="flex space-x-6">
+                        <li>
+                            <Link
+                                to="/"
+                                className="text-gray-200 font-semibold hover:text-yellow-400 transition-colors duration-200"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/card"
+                                className="text-gray-200 font-semibold hover:text-yellow-400 transition-colors duration-200"
+                            >
+                                Card Detail
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
