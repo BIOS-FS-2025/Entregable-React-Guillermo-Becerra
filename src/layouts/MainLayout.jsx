200b116e468aa80f6a2fd5b98ea2cd509393eb52
guillermobecerra <guillebvb@gmail.com>
2025-08-31 23:15:06 -0300

    updated bg color layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 function MainLayout() {
 
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col bg-gray-200">
             <Navigation />
             <main className="flex-grow">
                 <Outlet />
